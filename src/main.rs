aba16a4432ba0a6862da2af3ac180ddc647d6694
Martijang <seungwoogo1123@gmail.com>
2025-10-10 10:05:06 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+
 #[path = "Editor.rs"]
 mod editor;
 
 use editor::Editor;
-fn main() -> iced::Result{
-    iced::application("Super simple Text Editor", Editor::update, editor::view).theme(|_s| iced::Theme::Dark).run()?;
-    Ok(())
+fn main() {
+    Editor::run().unwrap();
 }
