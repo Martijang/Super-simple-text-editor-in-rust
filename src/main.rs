f7c893be652281bed6dbce7e6cf8342a524d2697
Martijang <seungwoogo1123@gmail.com>
2025-10-30 18:16:51 +0800

    Added unwrap_or_else in main function.

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ mod editor;
 
 use editor::Editor;
 fn main() {
-    Editor::run("Super Simeple Text Editor").unwrap();
+    Editor::run("Super Simeple Text Editor").unwrap_or_else(|e| eprintln!("{e:?}"));
 }
